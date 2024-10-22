41522890d1905b95b98b769644b8f7566434811a
deng4 <dnsoika@gmail.com>
2024-10-22 11:34:56 +0200

    null_resource check added

--- terraform_associate/local_env/data.tf
+++ terraform_associate/local_env/data.tf
@@ -21,3 +21,7 @@ data "aws_vpc" "main" {
 data "aws_security_group" "my_ec2_sg" {
   id = "sg-8af416e8"
 }
+
+data "template_file" "user_data" {
+  template = file("./user-data.yaml")
+}
--- terraform_associate/local_env/main.tf
+++ terraform_associate/local_env/main.tf
@@ -10,22 +10,35 @@ resource "aws_instance" "web" {
   key_name               = aws_key_pair.deployer.key_name
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+  user_data = data.template_file.user_data.rendered
 
-  provisioner "local-exec" {
-    command = "echo ${self.public_ip} >> public_ip.txt"
-  }
+  # provisioner "local-exec" {
+  #   command = "echo ${self.public_ip} >> public_ip.txt"
+  # }
 
-  provisioner "remote-exec" {
-    inline = [ 
-      "echo ${aws_instance.web.private_ip} >> /home/ubuntu/ec2_private_ip"
-     ]
-     connection {
-       type = "ssh"
-       user = "ubuntu"
-       host = "${self.public_ip}"
-       private_key = "${file("~/.ssh/id_ed25519")}"
-     }
-  }
+  # provisioner "file" {
+  #   content     = "ami user: ${self.ami}"
+  #   destination = "/home/ubuntu/barsoon.txt"
+
+  #   connection {
+  #     type        = "ssh"
+  #     user        = "ubuntu"
+  #     host        = self.public_ip
+  #     private_key = file("~/.ssh/id_ed25519")
+  #   }
+  # }
+
+  # provisioner "remote-exec" {
+  #   inline = [ 
+  #     "echo ${aws_instance.web.private_ip} >> /home/ubuntu/ec2_private_ip"
+  #    ]
+  #    connection {
+  #      type = "ssh"
+  #      user = "ubuntu"
+  #      host = "${self.public_ip}"
+  #      private_key = "${file("~/.ssh/id_ed25519")}"
+  #    }
+  # }
 
   tags = {
     Name = "my_local_env_ec2"
